aa168d52077a202e6ada3c79abe82640cf6dee0f
Thanes Sriamornruttanakul <benz.thanes@gmail.com>
2022-04-29 22:34:06 +0700

    update plugin

--- app/src/main/java/com/coin/coinapplication/AndroidApplication.kt
+++ app/src/main/java/com/coin/coinapplication/AndroidApplication.kt
@@ -1,30 +1,17 @@
 package com.coin.coinapplication
 
-import android.app.Activity
 import android.app.Application
-import android.app.Service
-import androidx.fragment.app.Fragment
 import com.coin.coinapplication.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.HasServiceInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
 class AndroidApplication : Application(),
-    HasActivityInjector,
-    HasSupportFragmentInjector,
-    HasServiceInjector {
+    HasAndroidInjector {
 
     @Inject
-    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
-
-    @Inject
-    lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
-
-    @Inject
-    lateinit var serviceDispatchingAndroidInjector: DispatchingAndroidInjector<Service>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
@@ -35,16 +22,6 @@ class AndroidApplication : Application(),
     }
 
 
-    override fun activityInjector(): AndroidInjector<Activity> {
-        return activityDispatchingAndroidInjector
-    }
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
-        return fragmentDispatchingAndroidInjector
-    }
-
-    override fun serviceInjector(): AndroidInjector<Service> {
-        return serviceDispatchingAndroidInjector
-    }
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
 }
