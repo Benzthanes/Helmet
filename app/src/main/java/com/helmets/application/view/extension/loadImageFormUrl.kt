9037be85f8702ca09f6fda88a9ba7dc11af9cceb
Thanes Sr <thanes.sriamornruttanakul@scbtechx.io>
2024-07-25 10:12:21 +0700

    migrate version

--- app/src/main/java/com/helmets/application/view/extension/loadImageFormUrl.kt
+++ app/src/main/java/com/helmets/application/view/extension/loadImageFormUrl.kt
@@ -1,25 +1,23 @@
 package com.helmets.application.view.extension
 
 import android.content.Context
-import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 
 fun ImageView.loadImageFormUrl(context: Context, url: String?, placeHolder: Int, error: Int) {
-    if (url != null && url.endsWith("svg")) {
-        GlideToVectorYou
-            .init()
-            .with(context)
-            .setPlaceHolder(placeHolder, error)
-            .load(Uri.parse(url), this);
-    } else {
+//    if (url != null && url.endsWith("svg")) {
+//        GlideToVectorYou
+//            .init()
+//            .with(context)
+//            .setPlaceHolder(placeHolder, error)
+//            .load(Uri.parse(url), this);
+//    } else {
         Glide.with(this.context)
             .load(url.orEmpty())
             .apply(RequestOptions().error(error).placeholder(placeHolder))
             .into(this)
-    }
+//    }
 
 }
 
